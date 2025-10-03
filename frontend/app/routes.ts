e98108ee8f0d958be8ec20b5b037dc2425cd6324
turboeliasson <njal.eliasson@heimby.no>
2025-10-03 18:49:45 +0200

    Added blank checkin page

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,12 +3,16 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 const RouteNames = {
   home: "home",
   about: "about",
+  checkin: "checkin",
 };
 
 export default [
   index("routes/home.tsx"),
   route("/about", "routes/about.tsx", {
     id: RouteNames.about,
+  }),
+  route("/checkin", "routes/checkin.tsx", {
+    id: RouteNames.checkin,
   }),
 ] satisfies RouteConfig;
 
